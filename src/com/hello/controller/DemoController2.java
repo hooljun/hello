358d1722a36227b528d7360f65b54fc6d33c526d
calwen <hooljun@163.com>
2017-03-19 11:57:58 +0800

    测试修改

---
+++ src/com/hello/controller/DemoController2.java
@@ -0,0 +1,24 @@
+package com.hello.controller;
+
+/**
+ * Created by calwen on 2017/3/18.
+ */
+public class DemoController2 {
+
+     public static  void main(String[] args){
+
+         System.out.print("baobaoff !p");
+         System.out.print("baobao fff!");
+
+
+         print();
+
+         System.out.print("hellll0000000000o demo!");
+     }
+
+     public static void print(){
+
+         System.out.print("baobao !");
+         System.out.print("efefe !");
+     }
+}
