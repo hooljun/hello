cd1b691db8d829e97401c924f9839b6a61bfcf0a
calwen <hooljun@163.com>
2017-03-19 12:00:03 +0800

    idea 项目测试

---
+++ src/com/hello/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.hello.controller;
+
+/**
+ * Created by calwen on 2017/3/18.
+ */
+public class DemoController {
+
+     public static  void main(String[] args){
+
+         System.out.print("baobaoff !p");
+         System.out.print("baobao fff!");
+
+
+         print();
+         System.out.print("hello demo!");
+     }
+
+     public static void print(){
+
+         System.out.print("baobao !");
+         System.out.print("efefe !");
+     }
+}
